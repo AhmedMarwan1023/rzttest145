34384bad48d29a267e4d74ea21d2d096fde8060a
AhmedMarwan1023 <44772647+AhmedMarwan1023@users.noreply.github.com>
2018-12-01 16:24:06 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -548,7 +548,17 @@ if (message.content.startsWith(prefix + 'help')) { /// This is The DMS Code Send
 『!delet <name> / مسح الشات او الرووم فويس』
 『❖!ccolors <number> / ينشا لك الوان مع كم الوان تبي』
    `,`
-
+       ***__Music orders__***
+**
+『${prefix}play / لتشغيل أغنية برآبط أو بأسم』
+『${prefix}skip / لتجآوز الأغنية الحآلية』
+『${prefix}pause / إيقآف الأغنية مؤقتا』
+『${prefix}resume / لموآصلة الإغنية بعد إيقآفهآ مؤقتا』
+『${prefix}vol / لتغيير درجة الصوت 100 - 0』
+『${prefix}stop / لإخرآج البوت من الروم』
+『${prefix}np / لمعرفة الأغنية المشغلة حآليا』
+『${prefix}queue / لمعرفة قآئمة التشغيل / الخاصية تحت الصيانة بسبب عدة مشاكل 』
+**
         ***__Games orders__***
  **       
 『!rps / حجر ورقة مقص』
