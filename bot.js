7b04dc6dfe299345aef87c8f196092d99c85a0bb
AhmedMarwan1023 <44772647+AhmedMarwan1023@users.noreply.github.com>
2018-12-01 16:02:00 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -2974,27 +2974,6 @@ client.on("guildMemberAdd", member => {
       
       }
       });
-client.on("ready", () => {
-  function lol() {
-    client.guilds.get('456907404697665567').roles.find("name", "Members").setColor("RANDOM");
-  };
-  setInterval(lol, 1600);
-});
-
-const moment = require("moment")
-client.on("guildMemberAdd", m => {
-    if (datediff(parseDate(moment(m.user.createdTimestamp).format('l')), parseDate(moment().format('l'))) < 8) {
-        m.ban();
-    };
-    function parseDate(str) {
-        var mdy = str.split('/');
-        return new Date(mdy[2], mdy[0]-1, mdy[1]);
-    };
-   
-    function datediff(first, second) {
-        return Math.round((second-first)/(1000*60*60*24));
-    };
-});
 
 var prefix = "!";
 client.on('message', message => {
